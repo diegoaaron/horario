4232a00d6b6a66a54eefb9739b4d6ab6e274255d
Diego Aarón <diegoaarondv@gmail.com>
2022-04-05 07:29:08 -0500

    update form 1

--- oficios/urls.py
+++ oficios/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('miformulario', views.mascota_view, name='mascota_view'),
+    path('busqueda_productos/', views.busqueda_productos, name='busqueda_productos'),
+    path('buscar/', views.buscar, name='buscar'),
 ]
